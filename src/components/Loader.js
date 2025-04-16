dbd9df58a289279d6f13aee9260b7e00679747a6
Eidal559 <eidaljuarbe@gmail.com>
2025-04-15 20:13:06 -0400

    fixes

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import MindFlowLogo from "../assets/Svgs/mindflow.svg";
 
 const Container = styled(motion.div)`
   position: absolute;
@@ -8,41 +9,25 @@ const Container = styled(motion.div)`
   left: 0;
   right: 0;
   bottom: 0;
-
   touch-action: none;
   overflow: hidden;
-
   width: 100vw;
   height: 100vh;
-
   z-index: 6;
-
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
   background-color: ${(props) => props.theme.body};
   color: ${(props) => props.theme.text};
+`;
 
-  svg {
-    width: 10vw;
-    height: auto;
-    overflow: visible;
-    stroke-linejoin: round;
-    stroke-linecap: round;
-
-    g {
-      path {
-        stroke: ${(props) => props.theme.text};
-      }
-    }
-  }
-
+const LogoImg = styled(motion.img)`
+  width: 10vw;
+  height: auto;
+  
   @media (max-width: 48em) {
-    svg {
-      width: 20vw;
-    }
+    width: 20vw;
   }
 `;
 
@@ -62,26 +47,24 @@ const textVariants = {
   },
   visible: {
     opacity: 1,
-
     transition: {
       duration: 1,
-      yoyo: Infinity, // repeats infinite times
+      yoyo: Infinity,
       ease: "easeInOut",
     },
   },
 };
 
-const pathVariants = {
+const logoVariants = {
   hidden: {
     opacity: 0,
-    pathLength: 0,
+    scale: 0.5,
   },
   visible: {
     opacity: 1,
-    pathLength: 1,
-
+    scale: 1,
     transition: {
-      duration: 2,
+      duration: 1.5,
       ease: "easeInOut",
     },
   },
@@ -102,24 +85,13 @@ const Loader = () => {
         duration: 2,
       }}
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        enableBackground="new 0 0 24 24"
-        height="48px"
-        viewBox="0 0 24 24"
-        width="48px"
-        fill="none"
-      >
-        <g></g>
-        <g>
-          <motion.path
-            variants={pathVariants}
-            initial="hidden"
-            animate="visible"
-            d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"
-          />
-        </g>
-      </svg>
+      <LogoImg 
+        src={MindFlowLogo} 
+        alt="MindFlow Logo"
+        variants={logoVariants}
+        initial="hidden"
+        animate="visible"
+      />
       <Text variants={textVariants} initial="hidden" animate="visible">
         MindFlow
       </Text>
